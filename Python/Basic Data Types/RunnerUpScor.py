9b6adbda62b00ddc1d9ced77296a5d6ae0b2dccd
Esra-Turk <74728647+Esra-Turk@users.noreply.github.com>
2022-05-15 20:59:46 +0300

    Basic Data Types

--- Python/Basic Data Types/RunnerUpScor.py
+++ Python/Basic Data Types/RunnerUpScor.py
@@ -1,4 +1,4 @@
-#Problem: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+#Problem:  https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
 
 n = int(input())
 arr = map(int, input().split())
