0c40a233bf0585e745e3bc8b7f0fa017ba4ce6cb
Esra Türk <74728647+Esra-Turk@users.noreply.github.com>
2022-08-21 23:58:52 +0300

    Update and rename loop.java to Loop.java

--- Java/Introduction/Loop.java
+++ Java/Introduction/Loop.java
@@ -8,14 +8,14 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
 
-public class Solution {
+public class Lopp {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
         for(int i = 1; i<= 10; i++) {
             int result = N * i;
-            System.out.println(N + " x " +i + " = "  +result);
+            System.out.println(N+ " x "+ i+ " = "+ result);
         }
         bufferedReader.close();
     }
